bffc169d08a7cb6ee1a81671b14e4490d879aeb5
Tiffany <xueying.zhou@dorabot.com>
2018-10-24 15:55:38 +0800

    图像的读取、显示和保存

--- maoxingyun/1/note-1.md
+++ maoxingyun/1/note-1.md
@@ -1,3 +1,34 @@
+## 头文件和命名空间
 
-编译：
+三行标配
+
+``` C++
+#include <opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+ 
+using namespace cv;
+```
+
+<br>
+
+## 关于Mat类型
+
+cv::Mat类是用于保存图像以及其他矩阵数据的数据结构。
+
+默认情况下，其尺寸为0，我们也可以指定初始尺寸。
+比如定义一个Mat类对象：
+
+``` 
+cv::Mat pic(320,640,cv::Scalar(100));
+```
+
+其实用到Mat对象最简单的代码就是：
+```
+Mat img = imread("test.jpg");
+```
+
+<br>
+
+## 编译
+
 g++ try.cpp `pkg-config opencv --cflags --libs`
---
+++ maoxingyun/3/test-3.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace cv;
+
+int main(){
+
+    // Mat image = imread("test.png", -1);
+    // namedWindow("test");
+    // imshow("test",image);
+    // waitKey(6000);
+
+
+    Mat image1 = imread("test.png");
+    Mat image2 = imread("test2.png");
+
+    // namedWindow("screen1");
+    // imshow("screen1",image1);
+
+    // namedWindow("screen2");
+    // imshow("screen2",image2);
+
+    // 定义感兴趣区域ROI
+    Mat imageROI;
+    //imageROI = image1(Rect(800, 350, image2.cols, image2.rows));
+    imageROI = image1(Range(350, 350 + image2.rows), Range(800, 800 + image2.cols));
+
+    addWeighted(imageROI, 0.5, image2, 0.3, 0., imageROI);
+
+    namedWindow("screen3");
+    imshow("screen3", imageROI);
+
+    waitKey(6000);
+
+    return 0;
+}
